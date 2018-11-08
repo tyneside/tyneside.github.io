ff250ecd3327fb686153f65d55284204eecd7b5a
Mark Goldsberry <markgoldsberry@Marks-MacBook-Pro.local>
2018-11-08 00:01:21 -0600

    New site initial push

--- scripts/script.js
+++ scripts/script.js
@@ -10,25 +10,70 @@
 //						.staggerFrom(".subtitle",0.5,{scale:0, ease: Back.easeOut.config(1.5)},0.2,"+0.6")
 //	
 //});
-$(function() {	
+$(document).ready(function(){
+    $('.art-album').slick({
+      dots: false,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      cssEase: 'linear',
+      adaptiveHeight: true,
+      autoplay: true,
+      autoplaySpeed: 4000,
+    });
+
+    $('.storyboard-album').slick({
+      dots: false,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      cssEase: 'linear',
+      adaptiveHeight: true,
+      autoplay: true,
+      autoplaySpeed: 3000,
+    });
+  
+        $(function() {
+          $('a[href*="#"]:not([href="#"])').click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+              var target = $(this.hash);
+              target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+              if (target.length) {
+                $('html, body').animate({
+                  scrollTop: target.offset().top-0
+                }, 500);
+                return false;
+              }
+            }
+          });
+        });
+		
+
+
+    
+    
+    
+});
+
+$(function(){	
 	
 
-		if (!sessionStorage.getItem('hasVisited')) {
-			var logoTl = new TimelineMax();
-				logoTl.set("#Clientwork",{opacity:0},"+=0.0")
-						.set("#name, #mobileNav",{y:-150})
-						.to("#name, #mobileNav",0.5,{y:0, ease: Back.easeOut.config(1.5)},"+0.2")
-						.to("#Clientwork",0.5,{opacity:1, ease:Sine.easeInOut},"+0.3")
-						.staggerFrom(".title",0.5,{scale:0, ease: Back.easeOut.config(1.5)},0.2,"+0.3")
-						.staggerFrom(".subtitle",0.5,{scale:0, ease: Back.easeOut.config(1.5)},0.2,"+0.5")
-
-		  sessionStorage.setItem('hasVisited', true); 
-		} else{
-			var titleTl = new TimelineMax();
-						titleTl.staggerFrom(".title",0.5,{scale:0, ease: Back.easeOut.config(1.5)},0.2,"+0.3")
-						.staggerFrom(".subtitle",0.5,{scale:0, ease: Back.easeOut.config(1.5)},0.2,"+0.5")
-						.staggerFrom("img",0.5,{opacity:0,ease: Sine.easeInOut},0.2,"+0.5")			
-		}
+//		if (!sessionStorage.getItem('hasVisited')) {
+//			var logoTl = new TimelineMax();
+//				logoTl.set("#Clientwork",{opacity:0},"+=0.0")
+//						.set("#name, #mobileNav",{y:-150})
+//						.to("#name, #mobileNav",0.5,{y:0, ease: Back.easeOut.config(1.5)},"+0.2")
+//						.to("#Clientwork",0.5,{opacity:1, ease:Sine.easeInOut},"+0.3")
+//						.staggerFrom(".title",0.5,{scale:0, ease: Back.easeOut.config(1.5)},0.2,"+0.3")
+//						.staggerFrom(".subtitle",0.5,{scale:0, ease: Back.easeOut.config(1.5)},0.2,"+0.5")
+//
+//		  sessionStorage.setItem('hasVisited', true); 
+//		} else{
+//			var titleTl = new TimelineMax();
+//						titleTl.staggerFrom(".title",0.5,{scale:0, ease: Back.easeOut.config(1.5)},0.2,"+0.3")
+//						.staggerFrom(".subtitle",0.5,{scale:0, ease: Back.easeOut.config(1.5)},0.2,"+0.5")
+//						.staggerFrom("img",0.5,{opacity:0,ease: Sine.easeInOut},0.2,"+0.5")			
+//		}
 	
 	
 	
@@ -36,22 +81,35 @@ $(function() {
         var mobileNavClose = document.getElementById("mobileNavClose");
         var mainNav = document.getElementById("mainNav");
 
-	
+//       document.getElementById("mobileNav").addEventListener("click", function() {
+//          		var navTl = new TimelineMax();
+//		   			navTl.set(mobileNav,{css:{display:'none'}});
+//		   			navTl.set(mobileNavClose,{css:{display:'inherit'}});
+//		   			navTl.to(mainNav,0.3,{css:{right:0},ease: Sine.easeInOut});
+//
+//       });
+//		
+//       document.getElementById("mobileNavClose").addEventListener("click", function() {
+//          		var navCloseTl = new TimelineMax();
+//					navCloseTl.set(mobileNavClose,{css:{display:'none'}});
+//					navCloseTl.set(mobileNav,{css:{display:'inherit'}});
+//					navCloseTl.to(mainNav,0.3,{css:{right:-300},ease: Sine.easeInOut});
+//
+//       });	
 
        document.getElementById("mobileNav").addEventListener("click", function() {
           		var navTl = new TimelineMax();
-		   			navTl.set(mobileNav,{css:{display:'none'}});
-		   			navTl.set(mobileNavClose,{css:{display:'inherit'}});
-		   			navTl.to(mainNav,0.3,{css:{right:0},ease: Sine.easeInOut});
+		   			navTl.to(mobileNav,0.2,{scale:0,ease: Back.easeIn.config(1.7)},"+=0.0")
+		   			     .to(mobileNavClose,0.2,{scale:1,ease: Back.easeOut.config(1.7)},"+=0.0")
+		   			     .to(mainNav,0.3,{css:{right:0},ease: Sine.easeInOut},"-=0.3");
 
        });
 		
        document.getElementById("mobileNavClose").addEventListener("click", function() {
           		var navCloseTl = new TimelineMax();
-					navCloseTl.set(mobileNavClose,{css:{display:'none'}});
-					navCloseTl.set(mobileNav,{css:{display:'inherit'}});
-					navCloseTl.to(mainNav,0.3,{css:{right:-300},ease: Sine.easeInOut});
-
+					navCloseTl.to(mobileNavClose,0.2,{scale:0,ease: Back.easeIn.config(1.7)},"+=0.0")
+		   			     .to(mobileNav,0.2,{scale:1,ease: Back.easeOut.config(1.7)},"+=0.0")
+		   			     .to(mainNav,0.3,{css:{right:-300},ease: Sine.easeInOut},"-=0.3");
        });
 		
            
